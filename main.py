abe5a52715c3e4e16fbe53b54ede02a712d84edc
gireeshabogineni <gireesha.bogineni@gmail.com>
2024-03-14 23:16:32 +0000

    added defensive programming to existing basic program

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 def calculation(x, y, operator):
 
-    if operator == '+':
+    try:
+        if operator == '+':
 
-        total = x + y
-        return total
-    elif operator == '-':
+            total = x + y
+            return total
+        elif operator == '-':
 
-        total = x - y
-        return total
-    elif operator == 'x':
+            total = x - y
+            return total
+        elif operator == 'x':
 
-        total = x * y
-        return total
-    elif operator == '/':
+            total = x * y
+            return total
+        elif operator == '/':
 
-        total = x / y
-        return total
-    
+            total = x / y
+            return total
+    except ZeroDivisionError:
+        print("You cannot divide by Zero.. ")
 
 num1 = float(input("please enter a number : "))
 num2 = float(input("please enter a number : "))
-operator = input("Enter the any one math symbol (+,-,x,/) : ")
+
+ops = ['+', '-', 'x', '/']
+
+while True:
+    operator = input("Enter the any one math symbol (+,-,x,/) : ")
+
+    if operator not in ops:
+        print(''' Your selected operator is not vaid.
+                Please use of the  math symbol (+,-,x,/) only ''')
+        continue
+    else:
+        print('Calculating...')
+        break
+
 
 calc = calculation(num1, num2, operator)
 print(calc)
