349acfa11e6fa895e9999dc2a7efb3720d93f718
gireeshabogineni <gireesha.bogineni@gmail.com>
2024-03-15 00:08:04 +0000

    added new functionality

--- main.py
+++ main.py
@@ -18,20 +18,24 @@ def calculation(x, y, operator):
 
             total = x / y
             return total
+        elif operator == '%':
+
+            total = x % y
+            return total
     except ZeroDivisionError:
         print("You cannot divide by Zero.. ")
 
 num1 = float(input("please enter a number : "))
 num2 = float(input("please enter a number : "))
 
-ops = ['+', '-', 'x', '/']
+ops = ['+', '-', 'x', '/','%']
 # implementing defensive programming using while loop to ensure user entering correct operator
 while True:
-    operator = input("Enter the any one math symbol (+,-,x,/) : ")
+    operator = input("Enter the any one math symbol (+,-,x,/,'%') : ")
 
     if operator not in ops:
         print(''' Your selected operator is not vaid.
-                Please use of the  math symbol (+,-,x,/) only ''')
+                Please use of the  math symbol (+,-,x,/,%) only ''')
         continue
     else:
         print('Calculating...')
